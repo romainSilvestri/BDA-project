00208668581428274c944eae0c06c92e6e7d8b05
unknown <olivier22061997@gmail.com>
2021-05-28 10:46:19 +0200

    add source

---
+++ spark/BDA_project/src/main/scala/Project.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.sql.SparkSession
+
+object Project {
+  def main(args : Array[String]): Unit ={
+    val spark = SparkSession.builder().master("local[1]").appName("SparkByExample").getOrCreate()
+    import spark.implicits._
+    val dataWithoutHeader = spark.read.option("inferSchema",true).option("header",false).csv("./data/kddcup.data.corrected")
+
+    val data = dataWithoutHeader.toDF("duration","protocol_type","service","flag","src_bytes","dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised","root_shell","su_attempted","num_root","num_file_creations","num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login","count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate","label")
+    data.select("label").groupBy("label").count().orderBy($"count".desc).show(25)
+  }
+}
